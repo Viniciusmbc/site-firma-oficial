908e2c38b90b1a923b76d9056be4d54cbc23d5c3
viniciuscacere <viniciuscaceres@gmail.com>
2023-05-04 22:25:53 -0300

    add span widht and height

--- app/components/Cards/CardsHeroSection.tsx
+++ app/components/Cards/CardsHeroSection.tsx
@@ -10,8 +10,9 @@ export default function CardsHeroSection({
   return (
     <article className=" border border-[#098A5B] bg-[#313131] max-w-[294px] w-full mb-8">
       {icon}
-      <div className=" border-b border-[#098A5B] pb-3 pl-4">
+      <div className=" pb-3 pl-4">
         <strong className=" text-xl uppercase">{title}</strong>
+        <span className=" w-10 h-1 bg-[#098A5B] block"></span>
       </div>
 
       <p className=" mt-4 px-4 pb-7 flex items-center">{description}</p>
